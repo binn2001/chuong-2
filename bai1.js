183b0d128ef946f2ad1ad8f81518ad43ee8f7ad0
binn2001 <97430212+binn2001@users.noreply.github.com>
2023-12-21 09:52:11 +0700

    Add files via upload

---
+++ bai1.js
@@ -0,0 +1,10 @@
+// Khai bao bien
+var hovaten = 'Dinh Nguyen Minh Nhien';
+var age = 22;
+var address = 'Nguyen Van Luong, F17 Go Vap';
+
+setTimeout(function() {
+    alert(hovaten);
+    alert(age);
+    alert(address);
+}, 2000);
---
+++ bai2.js
@@ -0,0 +1,5 @@
+var hoTen = prompt("Nhập họ tên: ");
+var age = prompt("Nhập tuổi: ");
+
+console.log(hoTen);
+console.log(age);
